4c572b77331681c710d5acad5014a061a524e9e8
Alex <devstar0718@gmail.com>
2021-07-03 17:08:31 +0300

    Return Stream Object after create a stream.

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 const Livepeer = require('./src/livepeer');
 
-const livepeerObject = new Livepeer('4ea227ff-e35e-4f12-8945-a4ae7973fa3f');
+async function test(){
+    const livepeerObject = await new Livepeer('4ea227ff-e35e-4f12-8945-a4ae7973fa3f');
+    console.log("Starting...");
+    const stream = await livepeerObject.Stream.create(
+        {
+            'name': 'test_stream',
+            'profiles': [
+                {
+                    'name': '720p',
+                    'bitrate': 2000000,
+                    'fps': 30,
+                    'width': 1280,
+                    'height': 720
+                },
+                {
+                    'name': '480p',
+                    'bitrate': 1000000,
+                    'fps': 30,
+                    'width': 854,
+                    'height': 480
+                },
+                {
+                    'name': '36p',
+                    'bitrate': 500000,
+                    'fps': 30,
+                    'width': 640,
+                    'height': 360
+                },
+            ]
+        });
+    console.log(stream.id);
+}
 
-console.log("Starting...");
+test();
 
-livepeerObject.Stream.create(
-    {
-        'name': 'test_stream',
-        'profiles': [
-            {
-                'name': '720p',
-                'bitrate': 2000000,
-                'fps': 30,
-                'width': 1280,
-                'height': 720
-            },
-            {
-                'name': '480p',
-                'bitrate': 1000000,
-                'fps': 30,
-                'width': 854,
-                'height': 480
-            },
-            {
-                'name': '36p',
-                'bitrate': 500000,
-                'fps': 30,
-                'width': 640,
-                'height': 360
-            },
-        ]
-    }).then((stream) => {
-    console.log(stream.data.id);
-});
 
--- src/stream/stream.js
+++ src/stream/stream.js
@@ -8,13 +8,51 @@ const Base = require('../base');
 const PATH = '/stream';
 
 class Stream extends Base {
-    create(params){
+    constructor(...params) {
         if(!params){
             return Promise.reject(
                 new Error('Params are required for creating a stream')
             );
         }
-        return this.http.post(PATH, params);
+        super(params[0]);
+        this.parent = params[0];
+        if (params.length >= 2) {
+            const data = params[1];
+            if (typeof data === 'object') {
+                this.name = data.name;
+                this.profiles = data.profiles;
+                this.id = data.id;
+                this.createdAt = data.createdAt;
+                this.streamKey = data.streamKey;
+                this.playbackId = data.playbackId;
+            }
+        }
+    }
+
+    async create(params) {
+        if (!params) {
+            return Promise.reject(
+                new Error('Params are required for creating a stream')
+            );
+        }
+        const response = await this.http.post(PATH, params);
+        console.log('Create a Stream: ' + response.statusText);
+        switch (Math.floor(response.status / 100)) {
+            case 2:
+                return new Stream(this.parent, response.data);
+            case 4:
+                return Promise.reject(
+                    new Error('Client Error, problem with the information provided')
+                );
+            case 5:
+                return Promise.reject(
+                    new Error('Internal Server Error, problem with Livepeer servers')
+                );
+            default:
+                return Promise.reject(
+                    new Error('Unknown Error')
+                );
+        }
     }
 }
 
